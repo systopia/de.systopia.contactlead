46ac5c5a3a6c64aca1006b83f536affd11d9e491
B. Endres <endres@systopia.de>
2020-05-29 11:34:52 +0200

    [#2] implementing xportx module

--- CRM/Xportx/Module/ContactLeads.php
+++ CRM/Xportx/Module/ContactLeads.php
@@ -79,11 +79,11 @@ class CRM_Xportx_Module_ContactLeads extends CRM_Xportx_Module
             $field_name = $field_spec['key'];
             if (in_array($field_name, ['from', 'to', 'is_enabled', 'is_important'])) {
                 // this is a lead metadata field
-                $selects[] = "{$lead_alias}.{$field_name} AS {$value_prefix}{$field_name}";
+                $selects[] = "GROUP_CONCAT({$lead_alias}.{$field_name}) AS {$value_prefix}{$field_name}";
             } else {
                 // this is a lead contact field
-                // TODO: process exceptions (like prefix, etc.)
-                $selects[] = "{$lead_contact_alias}.{$field_name} AS {$value_prefix}{$field_name}";
+                // TODO: process exceptions (like individual_prefix, etc.)
+                $selects[] = "GROUP_CONCAT({$lead_contact_alias}.{$field_name}) AS {$value_prefix}{$field_name}";
             }
         }
     }
